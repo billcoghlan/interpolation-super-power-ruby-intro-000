d54967afaa9ff723e8b0ea09f4915b4800456e7a
Bill Coghlan <bill.coghlan1@gmail.com>
2019-02-03 06:33:19 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,6 @@
 # Write your #display_rainbow method here
-def display_rainbow
-  colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
-  puts "#{colors[0].String.upcase}: #{colors[0]}"
+def display_rainbow(color)
+  puts "R: #{color[0]}, 
+#  puts "#{colors[0].String.upcase}: #{colors[0]}"
 #  "x.String.upcase: x"
 end
