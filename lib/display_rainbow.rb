bc6532ce7c420b7c9585df74f8a19eeea48f6f4e
Bill Coghlan <bill.coghlan1@gmail.com>
2019-02-03 06:39:21 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,4 @@
 # Write your #display_rainbow method here
 def display_rainbow(color)
-  puts "R: #{color[0]}, O: #{color[1]}, Y: #{color[2]}, G: #{color[3]}, B: #{color[4]}, I: #{color[5]}, V: #{color[6]}"
-#  puts "#{colors[0].String.upcase}: #{colors[0]}"
-#  "x.String.upcase: x"
+  puts "R: #{color[0]}, O: #{color[1]}, Y: #{color[2]}, G: #{color[3]}, B: #{color[4]}, I: #{color[5]}, V: #{color[6]}""
 end
